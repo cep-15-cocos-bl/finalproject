f52f60d7e14a8b1804656aa2095b1c67ffdddb24
bjuniano <bjuniano@gmail.com>
2015-06-11 16:48:52 +0800

    Update buttonLayer.js
    touch handler removed (shifted to app.js)

--- src/buttonLayer.js
+++ src/buttonLayer.js
@@ -14,31 +14,10 @@ var buttonLayer = cc.Layer.extend({
         this.addChild(btnLeft);
         this.addChild(btnRight);
 
-        var listener = cc.EventListener.create({
-            event: cc.EventListener.TOUCH_ONE_BY_ONE,
-            swallowTouches: true,
-            onTouchBegan: function (touch, event) {
-                var target = event.getCurrentTarget();
-                var location = target.convertToNodeSpace(touch.getLocation());
-                var targetSize = target.getContentSize();
-                var targetRectangle = cc.rect(0, 0, targetSize.width, targetSize.height);
-                if (cc.rectContainsPoint(targetRectangle, location)) {
-                    target.processInput(touch.getLocationX(), touch.getLocationY());
-                }
-            }
-        });
 
-        cc.eventManager.addListener(listener, this);
+
     },
 
-    processInput: function(x, y) {
-
-        if(x >= 15 && x <= 45 && y >= 15 && y <= 45) {
-            console.log("left");
-        } else if(x >= 75 && x <= 105 && y >= 15 && y <= 45) {
-            console.log("right");
-        }
-    }
 })
 
 var leftButton = cc.Sprite.extend({
