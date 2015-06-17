1c05c552c02d2b58ecbd24518692ec34bd5be4f5
Shrek <bjuniano@gmail.com>
2015-06-17 13:24:33 +0800

    changes
    changes

--- src/collision.js
+++ src/collision.js
@@ -12,31 +12,12 @@ var collisionHandler = {
         } else if(arbiter.a.collision_type == "ground" && arbiter.b.collision_type == "gravline" ||
             arbiter.b.collision_type == "ground" && arbiter.a.collision_type == "gravline") {
             return false;
-        } else if(arbiter.a.collision_type == "trinket" && arbiter.b.collision_type != "player" ||
-            arbiter.b.collision_type == "trinket" && arbiter.a.collision_type != "player") {
-            return false;
-        } else if(arbiter.a.collision_type == "ground" && arbiter.b.collision_type == "player" ||
-            arbiter.b.collision_type == "ground" && arbiter.a.collision_type == "player") {
-
-            // enable flipping
-
-            return true;
         }
 
         return true;
     },
 
     preCollision: function(arbiter, space) {
-        if(arbiter.a.collision_type == "player" && arbiter.b.collision_type == "trinket") {
-            space.env.graveyard.push(arbiter.b);
-        } else if(arbiter.b.collision_type == "player" && arbiter.a.collision_type == "trinket") {
-            space.env.graveyard.push(arbiter.a);
-        } else if(arbiter.a.collision_type == "player" && (arbiter.b.collision_type == "spike" || arbiter.b.collision_type == "enemy")) {
-            space.env.graveyard.push(arbiter.a);
-        } else if(arbiter.b.collision_type == "player" && (arbiter.a.collision_type == "spike" || arbiter.a.collision_type == "enemy")) {
-            space.env.graveyard.push(arbiter.b);
-        }
-
         return true;
     },
 
@@ -45,18 +26,12 @@ var collisionHandler = {
             arbiter.a.decaying = true;
         } else if(arbiter.b.collision_type == "crumbling" && arbiter.a.collision_type == "player") {
             arbiter.b.decaying = true;
-        } else if(arbiter.a.collision_type == "playr" && arbiter.b.collision_type == "gravline" ||
+        } else if(arbiter.a.collision_type == "player" && arbiter.b.collision_type == "gravline" ||
             arbiter.b.collision_type == "player" && arbiter.a.collision_type == "gravline") {
             space.invertGravity();
         }
     },
 
-    endCollision: function(arbiter, space) {
-        if(arbiter.a.collision_type == "ground" && arbiter.b.collision_type == "player" ||
-            arbiter.b.collision_type == "ground" && arbiter.a.collision_type == "player") {
-
-            // disable flipping
-        }
-    }
+    endCollision: null
 
 };
